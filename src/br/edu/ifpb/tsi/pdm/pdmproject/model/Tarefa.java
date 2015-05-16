c64bc62091a586cb21133696892395d8d0edccac
Artur Araújo <arturaraujo.b@gmail.com>
2015-05-16 12:44:39 -0300

    Created model classes

---
+++ src/br/edu/ifpb/tsi/pdm/pdmproject/model/Tarefa.java
@@ -0,0 +1,52 @@
+package br.edu.ifpb.tsi.pdm.pdmproject.model;
+
+import java.util.Calendar;
+
+public class Tarefa {
+	private int id;
+	private Atividade atividade;
+	private Disciplina disciplina;
+	private Calendar dataHora;
+	private Calendar dataHoraNotificacao;
+
+	public Calendar getDataHoraNotificacao() {
+		return dataHoraNotificacao;
+	}
+
+	public void setDataHoraNotificacao(Calendar dataHoraNotificacao) {
+		this.dataHoraNotificacao = dataHoraNotificacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Atividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(Atividade atividade) {
+		this.atividade = atividade;
+	}
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public Calendar getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(Calendar dataHora) {
+		this.dataHora = dataHora;
+	}
+
+}
